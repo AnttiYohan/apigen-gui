e918b63c4c0e96fd9ab2e847bf3b41330e87d259
Antti Lappalainen <antti@prototyper.tech>
2021-08-04 19:05:32 +0300

    Initial commit, drop down selector of available mysql field types

---
+++ js/field-components/TypeSelector.js
@@ -0,0 +1,47 @@
+import { SelectorBase } from '../SelectorBase.js';
+
+/**
+ * This is a base class for a dropdown selector
+ */
+class TypeSelector extends SelectorBase
+{
+    constructor()
+    {
+        list = [
+
+            'int',
+            'bit',
+            'blob',
+            'char',
+            'float',
+            'boolean',
+            'varchar',
+            'datetime',
+            'timestamp',
+            'mediumtext'
+
+        ];
+        
+        super({ list });
+    }
+
+    // ----------------------------------------------
+    // - Lifecycle callbacks
+    // ----------------------------------------------
+
+    connectedCallback()
+    {
+        console.log( '<type-selector> connected' );
+        this.emit( 'type-selector-connected' );
+    }
+
+    disconnectedCallback()
+    {
+        console.log( '<type-selector> disconnected' );
+        //this.emit( 'schema-entry-removed' );
+    }
+}
+ 
+window.customElements.define( 'type-selector', TypeSelector );
+
+export { TypeSelector };
