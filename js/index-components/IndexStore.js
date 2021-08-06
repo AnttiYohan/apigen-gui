5841ec6ce42e7f33784c1fe5b81eec1978abcfb9
Antti Lappalainen <antti@prototyper.tech>
2021-08-06 07:45:25 +0300

    Initial commit, store component for indices

---
+++ js/index-components/IndexStore.js
@@ -0,0 +1,125 @@
+import { StoreComponent } from "../StoreComponent.js";
+import validate from "../util/validator.js";
+
+/**
+ * This is a store for IndexEntry objects.
+ * The indices in this store represent
+ * one database table, and its indices.
+ * 
+ * properties:
+ * - indices : array
+ * 
+ */
+class IndexStore extends StoreComponent
+{
+    constructor( options = {} )
+    {
+        const template = 
+       `<div class='component__create'>
+            <p class='component__label'>New Index</p>
+            <button class='component__action--create'></button> 
+        </div>`;
+
+        const style = 
+        `.component--root {
+            padding: 0;
+        }
+        .component__row {
+            padding: 8px;
+        }
+        .component__create {
+            padding: 8px;
+            justify-content: flex-end;
+        }`
+
+        super( { template, style } );
+
+        /**
+         * @property mReferences
+         * an array of stored reference entries
+         */
+        this.mIndices = [];
+
+        if ( 'indices' in options && Array.isArray( options.indices ) )
+        {
+            for ( const index of options.indices )
+            {
+                this.mIndices.push( index );
+            }
+        }
+
+        const createButton = this.shadowRoot.querySelector( '.component__action--create' );
+        createButton.addEventListener( 'click', e => this.addReference() );
+
+    }
+
+    /**
+     * Iterate though the entries and return the valid ones,
+     * If no valid entries, return undefined
+     * 
+     * @return {Array<object>|undefined}
+     */
+    get value()
+    {
+        const result = [];
+
+        for ( const entry of this.entries )
+        {
+            const model = 
+            [
+                {
+                    prop:  'fields',
+                    type:  'array',
+                    elem_type: 'string',
+                    min: 1
+                },
+                {
+                    prop: 'table',
+                    type: 'string',
+                    min: 1
+                }
+            ];
+
+            if ( validate( entry, model ) )
+            {
+                result.push( entry );
+            }
+        }
+
+        return result.length ? result : undefined;
+    }
+
+    /**
+     * Adds a class into the image area element, to display
+     * a red border -- when ensure is set,
+     * the notification fires only when the input is not set
+     * ------
+     * @param {boolean} ensure
+     */
+    notifyRequired( ensure = true )
+    {
+        return '';
+    }
+    
+    addIndex( index = {} )
+    {
+        this.addEntry( new IndexEntry( index ) );
+    }
+    // ----------------------------------------------
+    // - Lifecycle callbacks
+    // ----------------------------------------------
+
+    /**
+     * 
+     * @emits index-store-connected
+     */
+    connectedCallback()
+    {
+        console.log( '<index-store> connected' );
+        this.emit( 'index-store-connected' );
+    }
+}
+
+window.customElements.define( 'index-store', IndexStore );
+
+export { IndexStore };
