80f1ead077b7bcba18b43278466426d837cbde0a
Antti Lappalainen <antti@prototyper.tech>
2021-08-06 04:16:55 +0300

    Popup display auto closing when the context is defocused. Listens to 'popup-close' events, emits: 'popup-open', 'popup-close' when the popup display is opened and closed

--- js/PopupBase.js
+++ js/PopupBase.js
@@ -28,7 +28,7 @@ class PopupBase extends WCBase
        `<link rel='stylesheet' href='assets/css/style.css'>
         <div class='popup' tabindex='0'>
             ${'template_selected' in options ? options.template_selected : ''}
-            <ul class='popup__display'>
+            <ul class='popup__display' tabindex='0'>
                 ${'template' in options ? options.template : ''}
             </ul>
         </div>`);
@@ -67,6 +67,9 @@ class PopupBase extends WCBase
         .popup__display.open {
             display: block;
         }
+        .popup__display:focus-within {
+            outline: 3px solid rgba(120, 100, 255, 0.5);
+        }
         .selector__item {
             cursor: pointer;
             height: ${this.mItemHeight}px;
@@ -86,52 +89,178 @@ class PopupBase extends WCBase
         this.mDisplay   = this.shadowRoot.querySelector( '.popup__display' );
         this.mPopup.addEventListener( 'click', e => 
         {
-            this.mDisplay.classList.toggle( 'open' );    
+            console.log( `mPopup CLICK, target: ${e.target.className}` );
+
+            // - Open, but do not close
+            if ( ! this.mDisplay.classList.contains( 'open' ) )
+            {
+                this.mDisplay.classList.add( 'open' );
+                this.mDisplay.focus(); 
+            } 
+        });
+
+    /*    this.mDisplay.addEventListener( 'click', e => 
+        {
+            const active = this.shadowRoot.activeElement;
+            console.log( '------');
+            console.log( '' );
+            console.log( `DISPLAY Click: ${e.target.localName}` );
+            console.log( '-' );
+            if ( active )
+                console.log( `Local Active element: ${active.localName}` );
+            else
+                console.log( `Last active: ${this.mActiveElement ? this.mActiveElement.localName : 'null'}, document active: ${document.activeElement.localName}`);
+            //this.mDisplay.classList.remove( 'open' );
+        }, true);*/
+
+        this.mActiveElement = null;
+
+        /**
+         * Setup a 'focusout' listener into the popup display
+         * When the focus is not within, the
+         * display will be closed
+         */
+        this.mDisplay.addEventListener( 'focusout', e => 
+        {
+            const active = this.shadowRoot.activeElement;
+            if ( active ) this.mActiveElement = active;
+
+
+            const focusWithin = this.mPopup.querySelector( '.popup__display:focus-within' );
+
+            if ( ! focusWithin )
+            {
+                console.log( 'Display focus NOT found' );
+
+                if ( active )
+                {
+                    console.log( `Active found, ${active.localName}` );
+                }
+                else 
+                {
+                    this.close();
+                }
+            }
         });
 
         let focus = false;
         
+        /**
+         * Handles the key events in this shadowRoot context
+         * 
+         * @param {Event} e 
+         */
         const handleKeyPress = e => {
 
-            if ( focus )
+            /**
+             * Enter is pressed,
+             * the intended outcomes:
+             * 1. Open the popup display if not open
+             * 2. Close the popup display if open,
+             * and the active element is the display
+             */
+            if ( e.keyCode === this.KEY_ENTER )
             {
-                if ( e.keyCode === this.KEY_ENTER )
+                if ( this.isOpen() )
                 {
-                    if ( ! this.isOpen() )
+                    if ( this.mDisplay.isSameNode( this.shadowRoot.activeElement ) )
                     {
-                        this.open();
+                        this.close();
                     }
                 }
+                else 
+                if ( focus )
+                {
+                    this.open();
+                }
             }
+    
         };
 
+        /**
+         * Listen to the popup main button 'focus' events,
+         * handle by setting the 'focus' flag,
+         * and by opening the popup display, if not open yet
+         */
         this.mPopup.addEventListener( 'focus', e => 
         {
+            console.log( `Parent FOCUS, latest active: ${this.mActiveElement ? this.mActiveElement.localName: 'null'}` );
             focus = true;
+            // - Close if display open
+            if ( this.isOpen() )
+            {
+                this.close();
+            }
         });
+
+        /**
+         * Listen to the popup main button 'blur' events,
+         * unset the 'focus' flag
+         */
         this.mPopup.addEventListener( 'blur', e => 
         {
             focus = false;
         });
 
+        /**
+         * Set the shadowRoot to listen for 'keyup' events
+         */
         this.shadowRoot.addEventListener( 'keyup', e => handleKeyPress( e ) );
     }
 
-    handleClick( e ) 
-    {
-        e.preventDefault();
-        e.stopPropagation();
-        this.mDisplay.classList.remove( 'open' );
-    }
-
+    /**
+     * Returns the state of the popup display
+     * 
+     * @return {boolean} 
+     */
     isOpen()
     {
         return this.mDisplay.classList.contains( 'open' );
     }
 
+    /**
+     * Opens the popup and focuses into
+     * the popup display 
+     * 
+     * @emits popup-open
+     */
     open()
     {
         this.mDisplay.classList.add( 'open' );
+        this.mDisplay.focus();
+        this.emit( 'popup-open' );
+    }
+
+    /**
+     * Closes the popup and focuses into the
+     * popup selector button
+     * 
+     * @emits popup-close
+     */
+    close()
+    {
+        this.mDisplay.classList.remove( 'open' );
+        if ( this.shadowRoot.activeElement && this.shadowRoot.activeElement.isSameNode( this.mDisplay ) )
+        {
+            this.mDisplay.blur();
+        }
+        this.mPopup.focus();
+        this.emit( 'popup-close' );
+    }
+
+    /**
+     * Handles an event, which is set in the concrete class
+     * 
+     * @param {Event} e 
+     */
+    popupReceiver( e )
+    {
+        console.log( `PopupBase received broadcast from ${e.target}` );
+
+        if ( e.detail )
+        {
+            console.table( detail );
+        }
     }
 }
  
